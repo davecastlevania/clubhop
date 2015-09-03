582b9d91acbcf39aa4b1df2b073ba35aab1aadbe
casataco <the.dave.castaneda@gmail.com>
2015-09-02 22:40:30 -0400

    Major Scaffolding
    - Rescaffolded events and menu
    - Got rid of bottom tabs. Ineffective UX.
    - Add Individual Views for each menu item.

--- www/js/app.js
+++ www/js/app.js
@@ -1,46 +1,9 @@
-// Ionic Starter App
+// Club Hop Prototype
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('clubhop', ['ionic'])
-
-app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/')
-
-  $stateProvider.state('home', {
-    url: '/home',
-    views: {
-      home: {
-        templateUrl: 'templates/home.html',
-        controller: 'ClubCtrl'
-      }
-    }
-  })
-
-  $stateProvider.state('types', {
-    url: '/types',
-    views: {
-      types: {
-        templateUrl: 'templates/types.html'
-      }
-    }
-  })
-
-  $stateProvider.state('settings', {
-    url: '/settings',
-    views: {
-      settings: {
-        templateUrl: 'templates/settings.html'
-      }
-    }
-  })
-
-
-
-})
-
-
+var app = angular.module('clubhop', ['ionic', 'clubhop.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -54,3 +17,83 @@ app.config(function($stateProvider, $urlRouterProvider) {
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('home', {
+    url: '/home',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+    .state('home.deals', {
+      url: '/deals',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/deals.html',
+          controller: 'ClubCtrl'
+        }
+      }
+    })
+    .state('home.account', {
+      url: '/account',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/account.html',
+        }
+      }
+    })
+    .state('home.about', {
+      url: '/about',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/about.html',
+        }
+      }
+    })
+    .state('home.contact', {
+      url: '/contact',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/contact.html',
+        }
+      }
+    })
+    .state('home.favs', {
+      url: '/favs',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/favs.html',
+        }
+      }
+    })
+    .state('home.invite', {
+      url: '/invite',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/invite.html',
+        }
+      }
+    })
+    .state('home.payment', {
+      url: '/payment',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/payment.html',
+        }
+      }
+    })
+    .state('home.security', {
+      url: '/security',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/security.html',
+        }
+      }
+    })
+
+    ;
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home/deals');
+});
